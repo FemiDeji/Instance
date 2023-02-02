85dd2336b063e2da1209ac9922593ae19f3e1084
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 14:45:23 -0800

    Initial commit

---
+++ src/Instance.java
@@ -0,0 +1,14 @@
+public class Instance{
+    public String csc; // Declared Instance Variable
+    public Instance(){
+        // Default Constructor
+        this.csc = "Introduction to Java Programming"; //  initializing Instance Variable
+    }
+    //Main Method
+    public static void /main(String[] args);
+        // Object Creation
+    Instance name = new Instance();
+    //Displaying O/P
+     //System.out.println("CSC name is:" + name.csc);
+    }
+
